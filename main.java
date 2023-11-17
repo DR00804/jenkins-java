64a7b26b2baec96b9b549bfa1167cb9d67d11e04
devrathour@gmail.com <rathoredev424@gmail.com>
2023-11-17 20:32:22 +0530

    first commit

---
+++ main.java
@@ -0,0 +1,8 @@
+// Your First Program
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is java program"); 
+        System.out.println("Output from java program");
+    }
+}
